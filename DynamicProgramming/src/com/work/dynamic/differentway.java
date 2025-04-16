e03e8cae30ffb6a1dc0a406b71a4c27fda61c95a
MasakaSance <150430820+MasakaSance@users.noreply.github.com>
2025-04-16 23:01:49 +0800

    Add files via upload

---
+++ DynamicProgramming/src/com/work/dynamic/differentway.java
@@ -0,0 +1,27 @@
+package com.work.dynamic;
+
+public class differentway {
+    public static void main(String[] args) {
+        System.out.println(diffway(3,7));
+    }
+
+    public static int diffway(int m,int n){
+        int[][] dp = new int[m][n];
+
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+}
